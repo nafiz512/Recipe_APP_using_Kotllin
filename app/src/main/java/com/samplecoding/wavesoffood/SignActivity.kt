1dc56dda7beaee85b40f22db83012d8b234b8a70
Nafiz Muntasir <nafizmuntasir512@gmail.com>
2025-05-13 17:59:22 +0600

    sign up complition

--- app/src/main/java/com/samplecoding/wavesoffood/SignActivity.kt
+++ app/src/main/java/com/samplecoding/wavesoffood/SignActivity.kt
@@ -1,20 +1,24 @@
 package com.samplecoding.wavesoffood
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.samplecoding.wavesoffood.databinding.ActivitySignBinding
 
 class SignActivity : AppCompatActivity() {
+    private val binding: ActivitySignBinding by lazy {
+        ActivitySignBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.alreadyhavebutton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
